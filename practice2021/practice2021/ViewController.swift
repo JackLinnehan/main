5221637f9588879622c5c0c32706631738d1e7b8
JackLinnehan <linnehjb@bc.edu>
2021-12-11 16:15:48 -0500

    BipTheGuy

---
+++ practice2021/practice2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice2021
+//
+//  Created by John Linnehan on 11/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
